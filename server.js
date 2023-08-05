a2cc5e336e1cde1d5e3b746ff7419bf3a9531052
Ross F <rfahan@gmail.com>
2023-08-04 19:23:07 -0600

    added main menu via inquirer

--- server.js
+++ server.js
@@ -10,3 +10,31 @@ const db = mysql.createConnetction({
 },
     console.log(`Connected to the employee_tracker_db database.`)
 );
+
+
+// Function to display main menu and handle user input
+async function menuPrompt() {
+    try {
+        // Extract via object deconstruction
+        const { selection } = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'selection',
+                message: 'What would you like to do?:',
+                choices: [
+                    'View all employees',
+                    'Add Employee',
+                    'View all roles',
+                    'Add a role',
+                    'View All Departments',
+                    'Add Department',
+                    'Exit'
+                ]
+            }
+        ]);
+    }
+
+    catch (err) {
+        console.error('Error:', err);
+    }
+}
